6b3059933a5fb0b44bd8c42d4e4171ea3679bc4d
jfcorugedo <jfcorugedo@gmail.com>
2019-02-10 23:00:28 +0100

    adds a simple timeout and connect this component with navigation lifecycle

--- TimeTracking.js
+++ TimeTracking.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import  { NavigationEvents } from 'react-navigation';
+import CoolButton from "./CoolButton";
 
 class TimeTracking extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            costPerDay: 100,
+            currentCost: 0,
+            startTime: Date.now(),
+            endTime: undefined,
+        };
+    }
+
+    tick() {
+        this.setState({
+            currentCost: this.state.currentCost + 10
+        }, () => {
+            this.timer = setTimeout(() => this.tick(), 1000)
+        });
+    }
 
     render() {
         return (
             <View style={ styles.container }>
-                <Text>Tracking time....</Text>
+                <NavigationEvents
+                    onWillFocus={payload => {
+                        console.log('will focus');
+                        this.timer = setTimeout(() => this.tick(), 1000);
+                    }}
+                    onDidFocus={payload => console.log('did focus')}
+                    onWillBlur={payload => {
+                        console.log('will blur');
+                        clearTimeout(this.timer);
+                    }}
+                    onDidBlur={payload => console.log('did blur')}
+                />
+                <Text>{ this.state.currentCost }</Text>
+                <CoolButton
+                    label={ 'End meeting' }
+                    action={ () => this.props.navigation.goBack() }
+                />
             </View>
         );
     }
