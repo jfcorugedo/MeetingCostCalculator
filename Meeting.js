3db5dad1dfaa95c065543d425bff272b2903c764
jfcorugedo <jfcorugedo@gmail.com>
2019-02-10 20:40:09 +0100

    extract our button to its own component so we can reuse it all over the app

--- Meeting.js
+++ Meeting.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import CoolButton from "./CoolButton";
 
 export default class Meeting extends React.Component {
 
     render() {
         return (<View style={styles.container}>
-            <TouchableOpacity onPress={()  => this.props.navigation.navigate('TimeTracking') }>
-                <View style={styles.startButton}>
-                    <Text style={styles.startText}>Start meeting</Text>
-                </View>
-            </TouchableOpacity>
+            <CoolButton
+                action={ ()  => this.props.navigation.navigate('TimeTracking') }
+                label={'Start meeting'}
+            />
         </View>);
     }
 }
@@ -21,15 +21,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    startButton: {
-        textAlign: 'center',
-        backgroundColor: '#2f95dc',
-        borderRadius: 10,
-    },
-    startText: {
-        color: 'white',
-        fontSize: 30,
-        padding: 10,
     },
 });
