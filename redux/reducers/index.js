0c50a9a225b79947330a23e4e774f286a5f458c6
jfcorugedo <jfcorugedo@gmail.com>
2019-02-24 20:33:26 +0100

    creates our root reducer that will combine all the reducers together

---
+++ redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import addAttendeeReducer from './addAttendeeReducer';
+
+const rootReducer = combineReducers({
+    addAttendeeReducer
+});
+
+export default rootReducer;
