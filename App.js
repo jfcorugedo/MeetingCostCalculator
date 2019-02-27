f4738f806051336dd8538747ee173aead5f67e05
jfcorugedo <jfcorugedo@gmail.com>
2019-02-27 22:17:51 +0100

    adds KeyboardAvoidingView wrapping AppNavigation so all the content is visible when the user open the keyboard

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
 import { createBottomTabNavigator, createAppContainer, createStackNavigator } from 'react-navigation';
 
 import TabBarIcon from './TabBarIcon';
@@ -70,7 +70,9 @@ class App extends React.Component {
     render() {
         return (
             <Provider store={store}>
-                <AppNavigation></AppNavigation>
+                <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
+                    <AppNavigation></AppNavigation>
+                </KeyboardAvoidingView>
             </Provider>
         )
     }
