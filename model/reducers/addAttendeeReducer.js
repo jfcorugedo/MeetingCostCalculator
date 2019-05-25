4d063c45e1a82968484c3072cbd930710c9a78d2
jfcorugedo <jfcorugedo@gmail.com>
2019-05-25 11:52:14 +0200

    adds a new reducer that knows how to deal with ADD_ATTENDEE actions

---
+++ model/reducers/addAttendeeReducer.js
@@ -0,0 +1,13 @@
+import { ADD_ATTENDEE } from '../actions/actions';
+
+const initialState = [{name: 'Juan', cost: '55'}, {name: 'Pablo', cost: '75'}];
+
+export default  (state = initialState, action) => {
+
+    switch(action.type){
+        case ADD_ATTENDEE:
+            return [...state, action.attendee];
+        default:
+            return state;
+    }
+};
