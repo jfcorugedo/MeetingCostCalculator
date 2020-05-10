2dc2bd3fbae5793b35091a93774da7fbf240b624
jfcorugedo <jfcorugedo@gmail.com>
2020-05-10 20:08:17 +0200

    upgrades to SDK 37

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View, KeyboardAvoidingView } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
+import { AppLoading } from 'expo';
+import * as Icon from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
 import AppNavigator from './navigation/AppNavigator';
 import rootReducer from './model/reducers';
 import {Provider} from 'react-redux';
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Platform} from 'react-native';
-import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createStackNavigator} from 'react-navigation-stack'
 
 import TabBarIcon from '../components/TabBarIcon';
 import Attendees from '../screens/Attendees';
